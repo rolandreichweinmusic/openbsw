756930f028c7d30c36b1d5fec6369b3523c3a524
Roland Reichwein <mail@reichwein.it>
2025-08-21 13:37:34 +0200

    Use -ffreestanding for C and C++

--- admin/cmake/ArmNoneEabi.cmake
+++ admin/cmake/ArmNoneEabi.cmake
@@ -21,10 +21,10 @@ set(C_COMMON_FLAGS
     "${CPU_BUILD_FLAGS} -mthumb -fshort-enums \
 -mno-unaligned-access -Wno-psabi \
 -fno-asynchronous-unwind-tables -fno-builtin -fno-common \
--ffunction-sections -fdata-sections -fstack-usage")
+-ffunction-sections -fdata-sections -fstack-usage -ffreestanding")
 
 set(CMAKE_ASM_FLAGS "-g -mcpu=cortex-m4")
-set(CMAKE_C_FLAGS "${C_COMMON_FLAGS} -ffreestanding")
+set(CMAKE_C_FLAGS "${C_COMMON_FLAGS}")
 set(CMAKE_CXX_FLAGS "${C_COMMON_FLAGS} -fno-rtti -fno-exceptions \
 -fno-non-call-exceptions -fno-threadsafe-statics -fno-use-cxa-atexit")
 set(CMAKE_EXE_LINKER_FLAGS "${CPU_BUILD_FLAGS} -static \
